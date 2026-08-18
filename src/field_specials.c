29e893238a870d6e636cca404c65c88aae9d235d
judicornadamsfoster <judicornadamsfoster@gmail.com>
2026-08-18 21:49:49 +0100

    set rival's starter
    sets rival's starter:
    if treecko chosen, rival gets torchic
    if torchic chosen, rival gets mudkip
    if mudkip chosen, rival gets treecko
    if any other starters are chosen, rival gets random of the 3 starters

--- src/field_specials.c
+++ src/field_specials.c
@@ -4268,3 +4268,27 @@ u8 Script_TryGainNewFanFromCounter(void)
 {
     return TryGainNewFanFromCounter(gSpecialVar_0x8004);
 }
+
+void CheckSpecies(void)
+{
+    u8 i;
+    u16 species;
+    struct Pokemon *pokemon;
+    for (i = 0; i < CalculatePlayerPartyCount(); i++)
+    {
+        pokemon = &gPlayerParty[i];
+        if (GetMonData(pokemon, MON_DATA_SANITY_HAS_SPECIES) && !GetMonData(pokemon, MON_DATA_IS_EGG))
+        {
+            species = GetMonData(pokemon, MON_DATA_SPECIES);
+            if (species == gSpecialVar_0x8005)
+            {
+                gSpecialVar_Result = TRUE;
+                return;
+            }
+            else
+            {
+                gSpecialVar_Result = FALSE;
+            }
+        }
+    }
+}
